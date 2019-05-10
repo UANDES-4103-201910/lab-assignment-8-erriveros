9958127ebd328305c1d810b7077f779bfbe374ec
erriveros <erriveros@miuandes.cl>
2019-05-09 22:34:09 -0400

    the inputs there but does nothing

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,8 +1,29 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
+class keyboardController{
+	constructor(keybordId){
+		this.target = keybordId
+	}
+
+	keyListnerLog(){
+		$('.key').click(function(){
+			console.log($(this).text());
+		});
+	}
+	
+}
+
 
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 	$('.key').click(function(){
 		console.log($(this).text());
+	})
+	$('#toggle').click(function(){
+		$('#kcontainer').toggle();
+	})
+	$('.key').click(function(){
+		if ($(this).text() === 'Bksp' && $(this).text() === 'Shift' && $(this).text() === 'Cancel' && $(this).text() === 'Accept'){
+			$('#input').append($(this).text())
+		}
 	})
 });
