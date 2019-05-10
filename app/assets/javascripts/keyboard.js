70fafa8ecd75371e2fb7bab30b0f9800a1005edb
erriveros <erriveros@miuandes.cl>
2019-05-09 21:57:52 -0400

    first part done

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,8 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
+
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	$('.key').click(function(){
+		console.log($(this).text());
+	})
 });
