8c0dbffb4052f85e7637bce1cdffac335e805b92
erriveros <erriveros@miuandes.cl>
2019-05-09 23:16:53 -0400

    thrid part almost ready

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,29 +1,36 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 class keyboardController{
-	constructor(keybordId){
-		this.target = keybordId
+	constructor(keybord){
+		this.target = keybord
 	}
 
-	keyListnerLog(){
-		$('.key').click(function(){
+	keyListnerLog(key){
+		$(key).click(function(){
 			console.log($(this).text());
 		});
 	}
-	
+
+	keyListnerInput(key){
+		$(key).click(function(){
+			if ($(this).text() !== 'Bksp' && $(this).text() !== 'Shift' && $(this).text() !== 'Cancel' && $(this).text() !== 'Accept'){
+				console.log('ye');
+				$('#input').append($(this).text());
+		}
+	});
+	}
+
+	toggleKeyboard(button){
+		$(button).click(function(){
+			$('#kcontainer').toggle();
+		})
+	}
 }
 
 
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
-	$('.key').click(function(){
-		console.log($(this).text());
-	})
-	$('#toggle').click(function(){
-		$('#kcontainer').toggle();
-	})
-	$('.key').click(function(){
-		if ($(this).text() === 'Bksp' && $(this).text() === 'Shift' && $(this).text() === 'Cancel' && $(this).text() === 'Accept'){
-			$('#input').append($(this).text())
-		}
-	})
+	const keyboard = new keyboardController('#kcontainer');
+	keyboard.keyListnerLog('.key')
+	keyboard.keyListnerInput('.key')
+	keyboard.toggleKeyboard('#toggle')
 });
